93acffbd96bae1fcf639365bbcb139fbc947b27d
Juan <juanvj0425@gmail.com>
2023-05-10 20:25:17 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,7 @@
+function hola(name) {
+    return("Hola " + name + "!");
+  }
+
+// console.log(hola("Pedro")) // "Hola Pedro!"
+// console.log(hola("Juan")) // "Hola Juan!"
+// console.log(hola("")) // "Hola !"
---
+++ js-1/sum.js
@@ -0,0 +1,7 @@
+function sum(int) {
+    return int * (int + 1)/2
+}
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
+console.log(sum(100))
